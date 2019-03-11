53e1c463695e5a5849c6fa9617c7679e4b5e9fbf
alexxsanya <alexxsanya@gmail.com>
2019-03-12 00:54:30 +0300

    [FINISHES #164372046] Project file system setup

---
+++ config.py
@@ -0,0 +1,27 @@
+from os import environ
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = environ['DATABASE_URL2']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "secret_key"
+    UPLOAD_FOLDER = "static/controls_img/"
+    ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+    MAX_CONTENT_LENGTH = 2 * 1024 * 1024 #max file size - 2MB
+
+class ProductionConfig(Config):
+    DEBUG = False
+    ENV = "production"
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    ENV = "development"
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
