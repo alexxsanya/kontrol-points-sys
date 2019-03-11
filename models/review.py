f38a1cbbd9aa2345a2bfbc399facd1db4e2b3565
alexxsanya <alexxsanya@gmail.com>
2019-03-12 01:38:39 +0300

    [#164423278] updated a data model for reviews

--- models/review.py
+++ models/review.py
@@ -6,55 +6,55 @@ class ReviewsModel(db.Model):
   """
     Reviews Models
   """ 
-  __tablename__ = 'reviews'
-id
-reviewed_by <fkey:user-id>
-date of review <timestamp>
-photo_evidence <blob>
-condition <destroyed,existing,good>
-review_details <text 255>
+    __tablename__ = 'reviews'
+    id
+    reviewed_by <fkey:user-id>
+    date of review <timestamp>
+    photo_evidence <blob>
+    condition <destroyed,existing,good>
+    review_details <text 255>
 
-  id = db.Column(db.Integer, primary_key=True)
-  reviewed_by = db.Column(db.String(128), nullable=False)
-  review_for = db.Column(db.String(128), nullable=False)
-  kontrol_condition = db.Column(db.String(128), nullable=False)
-  review_details = db.Column(db.Text,nullable=False)
-  created_at = db.Column(db.DateTime)
-  modified_at = db.Column(db.DateTime)
+    id = db.Column(db.Integer, primary_key=True)
+    reviewed_by = db.Column(db.String(128), nullable=False)
+    review_for = db.Column(db.String(128), nullable=False)
+    kontrol_condition = db.Column(db.String(128), nullable=False)
+    review_details = db.Column(db.Text,nullable=False)
+    created_at = db.Column(db.DateTime)
+    modified_at = db.Column(db.DateTime)
 
-  reviews = db.relationship('UserModel', backref='reviews', lazy=True)
+    reviews = db.relationship('UserModel', backref='reviews', lazy=True)
 
-  def __init__(self, data): 
-    self.reviewed_by = data.get('reviewed_by') 
-    self.review_for = data.get('review_for') 
-    self.kontrol_condition = data.get('kontrol_condition') 
-    self.review_details = data.get('review_details') 
-    self.created_at = datetime.datetime.utcnow()
-    self.modified_at = datetime.datetime.utcnow()
+    def __init__(self, data): 
+        self.reviewed_by = data.get('reviewed_by') 
+        self.review_for = data.get('review_for') 
+        self.kontrol_condition = data.get('kontrol_condition') 
+        self.review_details = data.get('review_details') 
+        self.created_at = datetime.datetime.utcnow()
+        self.modified_at = datetime.datetime.utcnow()
 
-  def save(self):
-    db.session.add(self)
-    db.session.commit()
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def update(self, data):
-    for key, item in data.items():
-      setattr(self, key, item)
-    self.modified_at = datetime.datetime.utcnow()
-    db.session.commit()
+    def update(self, data):
+        for key, item in data.items():
+            setattr(self, key, item)
+        self.modified_at = datetime.datetime.utcnow()
+        db.session.commit()
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
-  @staticmethod
-  def get_reviews_from_db():
-    return KontrolsModel.query.all()
-  @staticmethod
-  def get_one_review(id):
-    return KontrolsModel.query.get(id)
+    @staticmethod
+    def get_reviews_from_db():
+        return KontrolsModel.query.all()
+    @staticmethod
+    def get_one_review(id):
+        return KontrolsModel.query.get(id)
 
-  def __repr__(self):
-    return '<id {}>'.format(self.id)
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
 
 class ReviewsModelSchema(Schema):
   """
