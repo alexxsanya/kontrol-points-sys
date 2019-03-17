c04178632d6db89caef42eb131e0485e1283a128
alexxsanya <alexxsanya@gmail.com>
2019-03-18 00:16:59 +0300

    [#164372046] Updated the static folder to hold images

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = environ['DATABASE_URL2']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "secret_key"
-    UPLOAD_FOLDER = "static/controls_img/"
+    UPLOAD_FOLDER = "static/assets/"
     ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
     MAX_CONTENT_LENGTH = 2 * 1024 * 1024 #max file size - 2MB
 
